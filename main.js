29fb1024aa0c0a5cc77a3504427278198f2784a3
tanveerns <tanveeralam229@gmail.com>
2022-07-25 22:16:21 +0530

    facade

--- main.js
+++ main.js
@@ -1,33 +1,51 @@
-
-var User = function (name) {
+var Mortgage = function (name) {
     this.name = name;
-
-    this.say = function () {
-        console.log("User: " + this.name);
-    };
 }
 
-var DecoratedUser = function (user, street, city) {
-    this.user = user;
-    this.name = user.name;  // ensures interface stays the same
-    this.street = street;
-    this.city = city;
+Mortgage.prototype = {
 
-    this.say = function () {
-        console.log("Decorated User: " + this.name + ", " +
-            this.street + ", " + this.city);
-    };
+    applyFor: function (amount) {
+        // access multiple subsystems...
+        var result = "approved";
+        if (!new Bank().verify(this.name, amount)) {
+            result = "denied";
+        } else if (!new Credit().get(this.name)) {
+            result = "denied";
+        } else if (!new Background().check(this.name)) {
+            result = "denied";
+        }
+        return this.name + " has been " + result +
+            " for a " + amount + " mortgage";
+    }
+}
+
+var Bank = function () {
+    this.verify = function (name, amount) {
+        // complex logic ...
+        return true;
+    }
+}
+
+var Credit = function () {
+    this.get = function (name) {
+        // complex logic ...
+        return true;
+    }
+}
+
+var Background = function () {
+    this.check = function (name) {
+        // complex logic ...
+        return true;
+    }
 }
 
 function run() {
+    var mortgage = new Mortgage("Joan Templeton");
+    var result = mortgage.applyFor("$100,000");
 
-    var user = new User("Kelly");
-    user.say();
-
-    var decorated = new DecoratedUser(user, "Broadway", "New York");
-    decorated.say();
+    console.log(result);
 }
-
 
 run();
 
