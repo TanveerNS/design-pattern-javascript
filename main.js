06a716b80c0d6c8ab8e78199c317cb15ef9a7eae
tanveerns <tanveeralam229@gmail.com>
2022-08-02 21:21:12 +0530

    command

--- main.js
+++ main.js
@@ -1,22 +1,78 @@
 
-var Request = function (amount) {
-    this.amount = amount;
-    console.log("Requested: $" + amount + "\n");
+function add(x, y) { return x + y; }
+function sub(x, y) { return x - y; }
+function mul(x, y) { return x * y; }
+function div(x, y) { return x / y; }
+
+var Command = function (execute, undo, value) {
+    this.execute = execute;
+    this.undo = undo;
+    this.value = value;
 }
 
-Request.prototype = {
-    get: function (bill) {
-        var count = Math.floor(this.amount / bill);
-        this.amount -= count * bill;
-        console.log("Dispense " + count + " $" + bill + " bills");
-        return this;
+var AddCommand = function (value) {
+    return new Command(add, sub, value);
+};
+
+var SubCommand = function (value) {
+    return new Command(sub, add, value);
+};
+
+var MulCommand = function (value) {
+    return new Command(mul, div, value);
+};
+
+var DivCommand = function (value) {
+    return new Command(div, mul, value);
+};
+
+var Calculator = function () {
+    var current = 0;
+    var commands = [];
+
+    function action(command) {
+        var name = command.execute.toString().substr(9, 3);
+        return name.charAt(0).toUpperCase() + name.slice(1);
+    }
+
+    return {
+        execute: function (command) {
+            current = command.execute(current, command.value);
+            commands.push(command);
+            console.log(action(command) + ": " + command.value);
+        },
+
+        undo: function () {
+            var command = commands.pop();
+            current = command.undo(current, command.value);
+            console.log("Undo " + action(command) + ": " + command.value);
+        },
+
+        getCurrentValue: function () {
+            return current;
+        }
     }
 }
-function run() {
-    var request = new Request(378);
 
-    request.get(100).get(50).get(20).get(10).get(5).get(1);
+function run() {
+
+    var calculator = new Calculator();
+
+    // issue commands
+
+    calculator.execute(new AddCommand(100));
+    calculator.execute(new SubCommand(24));
+    calculator.execute(new MulCommand(6));
+    calculator.execute(new DivCommand(2));
+
+    // reverse last two commands
+
+    calculator.undo();
+    calculator.undo();
+
+    console.log("\nValue: " + calculator.getCurrentValue());
 }
+
 
 run();
 
