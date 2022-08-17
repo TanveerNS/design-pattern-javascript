3f5f915701b4c6b41e6873915e55038f41d5cc37
tanveerns <tanveeralam229@gmail.com>
2022-08-17 23:55:09 +0530

    observer

--- main.js
+++ main.js
@@ -1,61 +1,45 @@
-var Person = function (name, street, city, state) {
-    this.name = name;
-    this.street = street;
-    this.city = city;
-    this.state = state;
+function Click() {
+    this.handlers = [];  // observers
 }
 
-Person.prototype = {
+Click.prototype = {
 
-    hydrate: function () {
-        var memento = JSON.stringify(this);
-        return memento;
+    subscribe: function (fn) {
+        this.handlers.push(fn);
     },
 
-    dehydrate: function (memento) {
-        var m = JSON.parse(memento);
-        this.name = m.name;
-        this.street = m.street;
-        this.city = m.city;
-        this.state = m.state;
+    unsubscribe: function (fn) {
+        this.handlers = this.handlers.filter(
+            function (item) {
+                if (item !== fn) {
+                    return item;
+                }
+            }
+        );
+    },
+
+    fire: function (o, thisObj) {
+        var scope = thisObj || window;
+        this.handlers.forEach(function (item) {
+            item.call(scope, o);
+        });
     }
-}
-
-var CareTaker = function () {
-    this.mementos = {};
-
-    this.add = function (key, memento) {
-        this.mementos[key] = memento;
-    },
-
-        this.get = function (key) {
-            return this.mementos[key];
-        }
 }
 
 function run() {
 
-    var mike = new Person("Mike Foley", "1112 Main", "Dallas", "TX");
-    var john = new Person("John Wang", "48th Street", "San Jose", "CA");
-    var caretaker = new CareTaker();
+    var clickHandler = function (item) {
+        console.log("fired: " + item);
+    };
 
-    // save state
+    var click = new Click();
 
-    caretaker.add(1, mike.hydrate());
-    caretaker.add(2, john.hydrate());
-
-    // mess up their names
-
-    mike.name = "King Kong";
-    john.name = "Superman";
-
-    // restore original state
-
-    mike.dehydrate(caretaker.get(1));
-    john.dehydrate(caretaker.get(2));
-
-    console.log(mike.name);
-    console.log(john.name);
+    click.subscribe(clickHandler);
+    click.fire('event #1');
+    click.unsubscribe(clickHandler);
+    click.fire('event #2');
+    click.subscribe(clickHandler);
+    click.fire('event #3');
 }
 
 run();
