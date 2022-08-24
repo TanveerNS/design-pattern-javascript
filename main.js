4abcdc6b3b545329d8ab9f3ea6cfcd6f77c71541
tanveerns <tanveeralam229@gmail.com>
2022-08-24 20:53:17 +0530

    visitor

--- main.js
+++ main.js
@@ -1,36 +1,62 @@
-var datastore = {
-    process: function () {
-        this.connect();
-        this.select();
-        this.disconnect();
-        return true;
-    }
+var Employee = function (name, salary, vacation) {
+    var self = this;
+
+    this.accept = function (visitor) {
+        visitor.visit(self);
+    };
+
+    this.getName = function () {
+        return name;
+    };
+
+    this.getSalary = function () {
+        return salary;
+    };
+
+    this.setSalary = function (sal) {
+        salary = sal;
+    };
+
+    this.getVacation = function () {
+        return vacation;
+    };
+
+    this.setVacation = function (vac) {
+        vacation = vac;
+    };
 };
 
-function inherit(proto) {
-    var F = function () { };
-    F.prototype = proto;
-    return new F();
-}
+var ExtraSalary = function () {
+    this.visit = function (emp) {
+        emp.setSalary(emp.getSalary() * 1.1);
+    };
+};
+
+var ExtraVacation = function () {
+    this.visit = function (emp) {
+        emp.setVacation(emp.getVacation() + 2);
+    };
+};
 
 function run() {
-    var mySql = inherit(datastore);
 
-    // implement template steps
+    var employees = [
+        new Employee("John", 10000, 10),
+        new Employee("Mary", 20000, 21),
+        new Employee("Boss", 250000, 51)
+    ];
 
-    mySql.connect = function () {
-        console.log("MySQL: connect step");
-    };
+    var visitorSalary = new ExtraSalary();
+    var visitorVacation = new ExtraVacation();
 
-    mySql.select = function () {
-        console.log("MySQL: select step");
-    };
+    for (var i = 0, len = employees.length; i < len; i++) {
+        var emp = employees[i];
 
-    mySql.disconnect = function () {
-        console.log("MySQL: disconnect step");
-    };
-
-    mySql.process();
+        emp.accept(visitorSalary);
+        emp.accept(visitorVacation);
+        console.log(emp.getName() + ": $" + emp.getSalary() +
+            " and " + emp.getVacation() + " vacation days");
+    }
 }
 
 run();
